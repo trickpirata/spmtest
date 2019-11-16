61ebc1e2e3dc676b7224b2a88e4053bcbd6b1865
Trick Gorospe <patrick.gorospe@icloud.com>
2019-11-16 17:17:30 +0800

    another one

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,7 +1,11 @@
 import UIKit
 public class MyLibrary {
 
-    public func showMeow(inVC viewController: UIViewController) {
+    init () {
+        
+    }
+    
+    public static unc showMeow(inVC viewController: UIViewController) {
         let a = UIAlertController(title: "Test", message: "Meow", preferredStyle: .alert)
         
         viewController.show(a, sender: nil)
